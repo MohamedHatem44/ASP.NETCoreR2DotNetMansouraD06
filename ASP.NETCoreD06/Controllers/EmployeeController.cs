d70ed371975c8b865a7bec9ab5265e43474cff4d
Mohamed Hatem <Mohamed.Hatem157@gmail.com>
2026-05-23 15:47:58 +0300

    Add project files.

---
+++ ASP.NETCoreD06/Controllers/EmployeeController.cs
@@ -0,0 +1,199 @@
+﻿using ASP.NETCoreD06.Data.Context;
+using ASP.NETCoreD06.Models;
+using ASP.NETCoreD06.ViewModels.Employee;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP.NETCoreD06.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        /*------------------------------------------------------------------*/
+        // Context => DB => Data Access
+        private readonly AppDbContext db = new AppDbContext();
+        /*------------------------------------------------------------------*/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            // Get All Employees
+            // Map From Domain Model To VM
+            var employeesReadVM = db.Employees
+                .Include(e => e.Department)
+                .Select(e => new EmployeeReadVM
+                {
+                    Id = e.Id,
+                    Name = e.Name,
+                    Age = e.Age,
+                    Salary = e.Salary,
+                    Department = e.Department!.Name
+                }).ToList();
+
+            return View(employeesReadVM);
+        }
+        /*------------------------------------------------------------------*/
+        // View Details
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            var employee = db.Employees
+                .Include(e => e.Department)
+                .FirstOrDefault(e => e.Id == id);
+
+            if (employee == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            // Map FromDomain Model To View Model
+            var employeeReadVM = new EmployeeReadVM
+            {
+                Id = employee.Id,
+                Name = employee.Name,
+                Age = employee.Age,
+                Salary = employee.Salary,
+                Department = employee.Department!.Name
+            };
+
+            return View(employeeReadVM);
+        }
+        /*------------------------------------------------------------------*/
+        // V01
+        // Create Employee
+        // Get => Show Form
+        [HttpGet]
+        public IActionResult CreateV01()
+        {
+            ViewBag.Departments = new SelectList(db.Departments, "Id", "Name");
+            return View();
+        }
+        /*------------------------------------------------------------------*/
+        // V01
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreateV01(Employee employee)
+        {
+            ModelState.Remove("Department");
+            // Validation
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Departments = new SelectList(db.Departments, "Id", "Name");
+                return View(employee);
+            }
+            db.Employees.Add(employee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        /*------------------------------------------------------------------*/
+        // V02
+        // Create Employee
+        // Get => Show Form
+        // Get Departments => For DropdownList
+        [HttpGet]
+        public IActionResult CreateV02()
+        {
+            var employeeCreateVM = new EmployeeCreateVM
+            {
+                Departments = GetDepartmentsForDropDown()
+            };
+            return View(employeeCreateVM);
+        }
+        /*------------------------------------------------------------------*/
+        // V02
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreateV02(EmployeeCreateVM employeeCreateVM)
+        {
+            // Validation
+            if (!ModelState.IsValid)
+            {
+                employeeCreateVM = new EmployeeCreateVM
+                {
+                    Departments = GetDepartmentsForDropDown()
+                };
+                return View(employeeCreateVM);
+            }
+
+            // employeeCreateVM => Don't Have Id
+            // Map From VM To Domain Model
+            var employee = new Employee
+            {
+                Name = employeeCreateVM.Name,
+                Age = employeeCreateVM.Age,
+                Salary = employeeCreateVM.Salary,
+                DepartmentId = employeeCreateVM.DepartmentId
+            };
+
+            db.Employees.Add(employee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        /*------------------------------------------------------------------*/
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var employee = db.Employees.Include(e => e.Department).FirstOrDefault(e => e.Id == id);
+            if (employee == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            // Map Domain Model To VM
+            var employeeEditVM = new EmployeeEditVM
+            {
+                Id = employee.Id,
+                Name = employee.Name,
+                Age = employee.Age,
+                Salary = employee.Salary,
+                DepartmentId = employee.DepartmentId,
+                DepartmentName = employee.Department!.Name,
+                Departments = GetDepartmentsForDropDown()
+            };
+            return View(employeeEditVM);
+        }
+        /*------------------------------------------------------------------*/
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(EmployeeEditVM employeeEditVM)
+        {
+            var employeeInDb = db.Employees.FirstOrDefault(e => e.Id == employeeEditVM.Id);
+            if (employeeInDb == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            // Map From VM To Domain Model
+            employeeInDb.Name = employeeEditVM.Name;
+            employeeInDb.Age = employeeEditVM.Age;
+            employeeInDb.Salary = employeeEditVM.Salary;
+            employeeInDb.DepartmentId = employeeEditVM.DepartmentId;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        /*------------------------------------------------------------------*/
+        public IActionResult Delete(int id)
+        {
+            var employee = db.Employees.FirstOrDefault(e => e.Id == id);
+            if (employee == null)
+            {
+                return RedirectToAction("Index");
+            }
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        /*------------------------------------------------------------------*/
+        // Helper Method
+        // DRY => Reusable Code => Don't Repeat Yourself
+        private List<SelectListItem> GetDepartmentsForDropDown()
+        {
+            return db.Departments
+             .Select(d => new SelectListItem
+             {
+                 Value = d.Id.ToString(),
+                 Text = d.Name
+             }).ToList();
+        }
+        /*------------------------------------------------------------------*/
+    }
+}
